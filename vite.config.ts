b2f23e911f1216fcb85c02e1f0adc0a8577f1335
Dude <dodovladimir90@gmail.com>
2026-09-03 12:58:23 +0300

    robot.txt done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { sitemap } from 'vite-plugin-sitemap';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'http://localhost:5173',
+      routes: [
+        '/',
+        '/shop',
+        '/about',
+        '/blog',
+        '/contact',
+      ],
+      changefreq: 'weekly',
+      priority: 0.8,
+    }),
+  ],
+});
